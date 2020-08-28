9c3125d18c666618db65244a0c8fea0175c8e8ff
Jisan Jung <jjjung13@gmail.com>
2020-08-27 23:28:10 -0400

    initiated chart js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Line } from "react-chartjs-2";
 
 export class Chart extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+    }
+
+    // fetch data from past 30 days
+    // push into state
+    componentDidMount() {
+        fetch("https://corona.lmao.ninja/v3/covid-19/historical/all")
+            .then(res => res.json())
+            .then(data => {
+                const keys = Object.keys(data.cases);
+                const values = Object.values(data.cases);
+                console.log(keys, values);
+            });
+    }
+
     render() {
         return (
             <section>
-                chart
+                <Line 
+                    width={100}
+                    height={50}
+                />
             </section>
         )
     }
