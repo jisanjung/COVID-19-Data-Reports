7d3d9ae50f3e00e5e38b4f2d059ab8c6b9590a58
Jisan Jung <jjjung13@gmail.com>
2020-08-31 23:43:32 -0400

    added flag next to country names

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Country = (props) => {
 
-    let { name, cases, deaths, recovered } = props;
+    let { name, cases, deaths, recovered, flag } = props;
 
     return (
         <tr className="flex flex-between">
-            <td className="country-name">{name}</td>
+            <td className="country-name"><div className="flag inline-block"><img src={flag} alt={`${name} Flag`}/></div>{name}</td>
             <td className="flex flex-end country-data"><span>{cases}</span></td>
             <td className="flex flex-end country-data"><span>{deaths}</span></td>
             <td className="flex flex-end country-data"><span>{recovered}</span></td>
