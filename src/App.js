205393d3d84f55b483c616cb7864804b00580d72
Jisan Jung <jjjung13@gmail.com>
2020-09-09 00:38:35 -0400

    removed sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
 import Main from './components/Main';
 import "./sass/main.scss";
 
 function App() {
   return (
     <div>
-      <Sidebar/>
       <Main/>
     </div>
   );
