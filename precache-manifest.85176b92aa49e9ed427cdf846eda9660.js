5a8344fd67a27490adf9e19902cae1ecb0bc7459
Jisan Jung <jjjung13@gmail.com>
2020-09-08 23:48:12 -0400

    Updates

---
+++ precache-manifest.85176b92aa49e9ed427cdf846eda9660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceed8f3eb82176490be307f02f9d5ddb",
+    "url": "/COVID-19-Data-Reports/index.html"
+  },
+  {
+    "revision": "52e8ec91265fc785df87",
+    "url": "/COVID-19-Data-Reports/static/css/main.7df30c6e.chunk.css"
+  },
+  {
+    "revision": "081085c4bbfa612f03ef",
+    "url": "/COVID-19-Data-Reports/static/js/2.db1133c1.chunk.js"
+  },
+  {
+    "revision": "efe373883032cd282f4d2c1b869deab3",
+    "url": "/COVID-19-Data-Reports/static/js/2.db1133c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e8ec91265fc785df87",
+    "url": "/COVID-19-Data-Reports/static/js/main.41ab7e94.chunk.js"
+  },
+  {
+    "revision": "a40355d8d0e95c89e4f7",
+    "url": "/COVID-19-Data-Reports/static/js/runtime-main.03c31d06.js"
+  },
+  {
+    "revision": "560383ea07b886d4a30811644ca49964",
+    "url": "/COVID-19-Data-Reports/static/media/undraw.560383ea.svg"
+  }
+]);
